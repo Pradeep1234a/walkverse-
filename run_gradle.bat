56e5aba297d792f68dfcb90b633bed1a22d6623e
Keshav T <maginbailmani@gmail.com>
2026-07-10 11:34:47 +0530

    Clear all repository files

--- run_gradle.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Program Files\JetBrains\PyCharm 2023.3.4\jbr"
-set "ANDROID_HOME=C:\Users\kesha\AppData\Local\Android\Sdk"
-set "ANDROID_SDK_ROOT=C:\Users\kesha\AppData\Local\Android\Sdk"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
-"C:\Users\kesha\.gradle\wrapper\dists\gradle-9.1.0-bin\9agqghryom9wkf8r80qlhnts3\gradle-9.1.0\bin\gradle.bat" %*
